d1d83836f7f3e66c88446a24f559421f8453e2ba
Balister-tech <76853270+Balister-tech@users.noreply.github.com>
2021-04-14 12:15:05 +0530

    Add files via upload
    updated task

--- index.js
+++ index.js
@@ -5,152 +5,133 @@ const json2csv = require("json2csv").Parser;
 const http = require("http");
 const url = require("url");
 
+const urlAll = {
+  TimesNewsUrl: "https://timesofindia.indiatimes.com/briefs",
+  BusinessUrl:
+    "https://timesofindia.indiatimes.com/business/india-businessspeed-at-which-india-adapts-has-always-amazed-me-capgemini-ceo/articleshow/81981043.cms",
+  FirstPostNewsUrl: "https://www.firstpost.com",
+  MoneyControlUrl: "https://www.moneycontrol.com/news/news-all",
+};
+const Header = {
+  TimesHeader:
+    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+  "accept-encoding": "gzip, deflate, br",
+  "accept-language": "en-US,en;q=0.9",
+  BusinessHeader:
+    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+  "accept-encoding": "gzip, deflate, br",
+  "accept-language": "en-US,en;q=0.9",
+  FirstPostHeader:
+    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+  "accept-encoding": "gzip, deflate, br",
+  "accept-language": "en-US,en;q=0.9",
+  MoneyControlHeader:
+    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+  "accept-encoding": "gzip, deflate, br",
+  "accept-language": "en-US,en;q=0.9",
+};
+
+const HtmlAll = {
+  NewsHeader: 'div[class="brief_box "] > h2',
+  NewsParagraph: 'div[class="brief_box "] > p',
+  BusinessHeader: 'div[class="_3lvqr clearfix"] > div',
+  BusinessParagraph: 'div[class="bgImg notloggedin bgImg scroll-up"]>p', // check
+  FirstPostHeader: 'div[class="title-wrap"]>h3',
+  FirstPostParagraph: 'div[class="title-wrap"]>p',
+  MoneyControlHeader: "#cagetory>li>h2",
+  MoneyControlParagraph: "#cagetory>li>p",
+};
+
 ///////////////////////////////////// TIMES OF INDIA//////////////////////////
-const URL = "https://timesofindia.indiatimes.com/briefs";
-let TimesNews = "";
-let news = async () => {
-  let timeData = [];
-  let paragraph = [];
-  let ImageArr = [];
-  let businessNews = [];
+
+let news = async (str) => {
+  var url1 = "";
+  var Header = "";
+  var HtmlHeader = "";
+  var HtmlParagraph = "";
+  if (str == "MoneyControl") {
+    url1 = `${urlAll.MoneyControlUrl}`;
+    Header = `${Header.MoneyControlHeader}`;
+    HtmlHeader = `${HtmlAll.MoneyControlHeader}`;
+    HtmlParagraph = `${HtmlAll.MoneyControlParagraph}`;
+  } else if (str == "TimesNews") {
+    url1 = `${urlAll.TimesNewsUrl}`;
+    Header = `${Header.TimesHeader}`;
+    HtmlHeader = `${HtmlAll.NewsHeader}`;
+    HtmlParagraph = `${HtmlAll.NewsParagraph}`;
+  } else if (str == "BusinessNews") {
+    url1 = `${urlAll.BusinessUrl}`;
+    Header = `${Header.BusinessHeader}`;
+    HtmlHeader = `${HtmlAll.BusinessHeader}`;
+    HtmlParagraph = `${HtmlAll.BusinessParagraph}`;
+  } else if (str == "FirstPostNews") {
+    url1 = `${urlAll.FirstPostNewsUrl}`;
+    Header = `${Header.FirstPostHeader}`;
+    HtmlHeader = `${HtmlAll.FirstPostHeader}`;
+    HtmlParagraph = `${HtmlAll.FirstPostParagraph}`;
+  }
+
+  const URL = url1;
+  const data = {
+    HeadContent: [],
+    Paragraph: [],
+    Img: [],
+  };
+
   const response = await request({
     uri: URL,
     headers: {
-      accept:
-        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-      "accept-encoding": "gzip, deflate, br",
-      "accept-language": "en-US,en;q=0.9",
+      accept: Header,
     },
     gzip: true,
   });
 
+  // Head
   let $ = cheerio.load(response);
-  $('div[class="brief_box "] > h2').each((_, l) => {
-    let head = $(l).text();
-    timeData.push(head);
+  $(HtmlHeader).each((_, l) => {
+    let head = $(l).text().trim();
+    data.HeadContent.push(head);
   });
 
-  $('div[class="brief_box "] > p').each((_, l) => {
-    let para = $(l).text();
-    paragraph.push(para);
+  // Paragraph
+  $(HtmlParagraph).each((_, l) => {
+    let para = $(l).text().trim();
+    data.Paragraph.push(para);
   });
 
-  $('div[class="brief_box "] a > div > img').each((_, l) => {
-    let image = $(l).attr("src");
-    ImageArr.push(image);
+  // Image Link
+  $("img").each((index, image) => {
+    var img = $(image).attr("src");
+    // var baseUrl = `${url}` + `/`;
+    // var Links = baseUrl + img;
+    data.Img.push(img);
   });
-
-  timeData.forEach((el, i) => {
-    businessNews.push({
-      HeaderContent: timeData[i],
-      ImageLink: ImageArr[i],
-      BriefNews: paragraph[i],
-    });
+  var obj = JSON.stringify(data);
+  fs.writeFileSync("./data.json", `${obj}`, function (err) {
+    if (err) return console.error(err);
   });
-  let j2cp = new json2csv();
-  const csv = j2cp.parse(businessNews);
-  TimesNews = csv;
-  // fs.writeFileSync("./timesIndiaNews.txt", csv, "utf-8");
-  // console.log("File written");
-  // console.log(businessNews);
+  console.log(data);
 };
-news();
 
-////////////////////////////////  Business News////////////
-const url1 =
-  "https://timesofindia.indiatimes.com/business/india-businessspeed-at-which-india-adapts-has-always-amazed-me-capgemini-ceo/articleshow/81981043.cms";
-
-let busNews = "";
-let BusinessNews = async () => {
-  let Business = [];
-  const response = await request({
-    uri: url1,
-    headers: {
-      accept:
-        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-      "accept-encoding": "gzip, deflate, br",
-      "accept-language": "en-US,en;q=0.9",
-    },
-    gzip: true,
-  });
-
-  let $ = cheerio.load(response);
-  $('div[class="_3lvqr clearfix"] > div').each((_, l) => {
-    let head = $(l).text();
-    Business.push(head);
-  });
-  // let j2cp = new json2csv();
-  // const csv1 = j2cp.parse(Business);
-  busNews = Business.join("");
-  // fs.writeFileSync("./BusinessIndiaNews.txt", Business, "utf-8");
-  // console.log(Business);
-};
-BusinessNews();
-
-///////////////////////////////////////////////// Mutual fund /////////////////////
-const url2 = "https://www.moneycontrol.com/mutualfundindia/";
-let mutual = "";
-const moneyControl = async () => {
-  let Business = [];
-  const response = await request({
-    uri: url2,
-    headers: {
-      accept:
-        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-      "accept-encoding": "gzip, deflate, br",
-      "accept-language": "en-US,en;q=0.9",
-    },
-    gzip: true,
-  });
-
-  let $ = cheerio.load(response);
-  const mutualFund = $('div[class="tableprfm PR"] > p').text();
-  mutual = mutualFund;
-  // console.log(mutualFund);
-};
-moneyControl();
-
-//////////////////////////////// money control news////////////////////////////////
-
-const url3 = "https://www.moneycontrol.com/news/news-all/";
-let moneyNews = "";
-const MoneyControlNews = async () => {
-  let Business = [];
-  const response = await request({
-    uri: url3,
-    headers: {
-      accept:
-        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-      "accept-encoding": "gzip, deflate, br",
-      "accept-language": "en-US,en;q=0.9",
-    },
-    gzip: true,
-  });
-
-  let $ = cheerio.load(response);
-  const MutualDataNews = $("[href]").text();
-  let j2cp = new json2csv();
-  const csv = JSON.stringify(MutualDataNews);
-  moneyNews = csv;
-  // moneyNews = MutualDataNews;
-  // console.log(MutualDataNews);
-};
-MoneyControlNews();
+// news("BusinessNews");
+// news("TimesNews");
+news("MoneyControl");
+// news("FirstPostNews");
 
 ///////////////// server
-const server = http.createServer((req, res) => {
-  const pathName = req.url;
-  if (pathName === "/" || pathName === "/timesofindianews") {
-    res.end(TimesNews);
-  } else if (pathName === "/businessnews") {
-    res.end(busNews);
-  } else if (pathName === "/moneymarket") {
-    res.end(moneyNews);
-  } else if (pathName === "/mutualfund") {
-    res.end(mutual);
-  }
-});
+// const server = http.createServer((req, res) => {
+//   const pathName = req.url;
+//   if (pathName === "/" || pathName === "/timesofindianews") {
+//     res.end(TimesNews);
+//   } else if (pathName === "/businessnews") {
+//     res.end(busNews);
+//   } else if (pathName === "/moneymarket") {
+//     res.end(moneyNews);
+//   } else if (pathName === "/mutualfund") {
+//     res.end(mutual);
+//   }
+// });
 
-server.listen(8000, "127.0.0.1", () => {
-  console.log("Listening on request on 8000");
-});
+// server.listen(8000, "127.0.0.1", () => {
+//   console.log("Listening on request on 8000");
+// });
